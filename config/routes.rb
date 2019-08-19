2549c3bf979622bdb2de0db7583ff4aab39cd446
Yoichi Toyota <y.toyota@xtone.co.jp>
2019-08-19 21:10:43 +0900

    Add user microposts.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: %i(edit)
   resources :password_resets,     only: %i(new create edit update)
+  resources :microposts,          only: %i(create destroy)
 
   root 'static_pages#home'
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -4,7 +4,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
     user = User.last
-    user.activation_token = User.new_token
+    user.send(:create_activation_digest)
+    user.save
     UserMailer.account_activation(user)
   end
 
