4cbcd45f2ada49c5ad5eaf3576445086c9c61f93
Yoichi Toyota <wisteria.r.s@gmail.com>
2019-08-25 03:28:46 +0900

    Fix typo.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV.fecth('SENDGRID_USERNAME') { '' },
+      :user_name      => ENV.fetch('SENDGRID_USERNAME') { '' },
       :password       => ENV.fetch('SENDGRID_PASSWORD') { '' },
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
