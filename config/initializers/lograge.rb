7635739ff62284546d190f0935b17b70e0c0e0a2
Yoichi Toyota <y.toyota@xtone.co.jp>
2019-08-30 13:38:55 +0900

    Add lograge-sql configuration to extract extecuted sql as single line.

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -6,4 +6,10 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     { time: Time.now.to_i }
   end
+  config.lograge_sql.extract_event = Proc.new do |event|
+    { name: event.payload[:name], duration: event.duration.to_f.round(2), sql: event.payload[:sql] }
+  end
+  config.lograge_sql.formatter = Proc.new do |sql_queries|
+    sql_queries
+  end
 end
