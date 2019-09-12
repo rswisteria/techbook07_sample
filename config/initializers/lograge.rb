a2f2171c25260b71e60af191957f7cd117dd7482
Yoichi Toyota <y.toyota@xtone.co.jp>
2019-09-12 15:44:43 +0900

    Modify lograge settins to use json format.

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -2,15 +2,28 @@ require 'lograge/sql/extension'
 
 Rails.application.configure do
   config.lograge.enabled = true
+  config.lograge.formatter = Lograge::Formatters::Json.new
 
   config.lograge.custom_options = lambda do |event|
     now = Time.zone.now
-    { unixtime: now.to_i, time: now.iso8601 }
+    exceptions = %w(controller action format id)
+    {
+      params: event.payload[:params].except(*exceptions),
+      unixtime: now.to_i,
+      time: now.iso8601,
+      exception: event.payload[:exception],
+      exception_object: event.payload[:exception_object],
+      backtrace: event.payload[:exception_object]&.backtrace
+    }
   end
   config.lograge_sql.extract_event = Proc.new do |event|
-    event.payload[:sql]
+    {
+      name: event.payload[:name],
+      duration: event.duration.to_f.round(2),
+      sql: event.payload[:sql]
+    }
   end
   config.lograge_sql.formatter = Proc.new do |sql_queries|
-    "'#{sql_queries.join("\\n")}'"
+    sql_queries
   end
 end
